ccdf9427bdf1d551144243fbc15526f1b727cd71
zaki-lknr <zaki.hmkc+github@gmail.com>
2024-10-03 16:20:12 +0900

    update: IGW/ルートテーブル定義をmodule化

--- aws/practice/modules/aws_sample/main.tf
+++ aws/practice/modules/aws_sample/main.tf
@@ -36,3 +36,29 @@ resource "aws_subnet" "prac_priv2" {
     Name = "private2-subnet-aws-study"
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.practice.id
+
+  tags = {
+    Name = "igw-aws-study"
+  }
+}
+
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.practice.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route-table-aws-study"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet" {
+  subnet_id      = aws_subnet.prac_public.id
+  route_table_id = aws_route_table.public_route.id
+}
